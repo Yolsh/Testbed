8b99b6b99402c2a3ca35d3c812667cb3592614a0
23JA1168 <23JA1168@L2774>
2023-10-17 11:29:38 +0100

    L114 string Manipulation

---
+++ L114 string manipulation/L114 string manipulation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L114_string_manipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input your first Name, your last name, the year you where born, your favourite colour, your street name and your shoe size.");
+            Console.WriteLine(Console.ReadLine()[0].ToString().ToUpper() + Console.ReadLine()[1].ToString().ToUpper() + Console.ReadLine().Substring(2, 2) + Console.ReadLine().Substring(1, 2) + Console.ReadLine().Substring(1, 3) + Console.ReadLine()[0]);
+            Console.ReadKey();
+        }
+    }
+}
