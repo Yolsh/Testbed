31976d33659bfa2d65578ebb9f7095bae0849df5
23JA1168 <23JA1168@L2774>
2023-10-20 16:01:41 +0100

    u

---
+++ L116 Debugging/L116 Debugging.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L116_Debugging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0; double num2 = 1;
+            double result = 0.0;
+            string exp, op, choice;
+            bool again = false;
+
+            while (again)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to the calculator program");
+                Console.WriteLine("Enter a maths expression (e.g. 5+6)");
+
+                exp = Console.ReadLine();
+
+                num1 = int.Parse(exp.Substring(0, 1));
+                num2 = double.Parse(exp.Substring(2, 1));
+                op = exp.Substring(2, 3);
+
+                switch (exp)
+                {
+                    case "+":
+                        result = num2 + num1;
+                        break;
+                    case "-":
+                        result = num2 - num1;
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                }
+
+                Console.WriteLine(exp + "=" + result);
+
+                Console.WriteLine("Would you like to go again? [Y/N]");
+                choice = Console.ReadKey().Key.ToString();
+                
+
+                if (choice.ToUpper() != "Y")
+                {
+                    again = true;
+                }
+                else
+                {
+                    again = false;
+                }
+            }
+
+            Console.WriteLine("Goodbye. Press any key to terminate the program");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ W108/W108.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W108
+{
+    internal class W108
+    {
+        static void oopsie(string[] list)
+        {
+            for (int i = list.Length; i > 0; i--)
+            {
+                Console.WriteLine(list[i]);
+            } 
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("which program?");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    int[] num = new int[8];
+
+                    for (int i = 0; i < num.Length; i++)
+                    {
+                        num[i] = 0;
+                        Console.WriteLine(num[i]);
+                    }
+                    break;
+
+                case 2:
+                    string[] names = { "Joe", "James", "Jonathan", "James", "Joel" };
+
+                    for (int i = 0; i < names.Length*2; i++)
+                    {
+                        (1 < 5) ? Console.WriteLine(names[i]) : oopsie(names);
+                    }
+
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
