15dc4d140f6e52893e4538fa81673fc204baa75b
23JA1168 <23JA1168@L2768>
2023-11-07 12:00:08 +0000

    W109

---
+++ L118 Parralel Arrays/L118 Parralel Arrays.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L118_Parralel_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Names = { "Fred", "Jack", "Chris", "Ali", "Harry", "Bill", "Zak", "Phil"};
+            int[] Marks = { 76, 25, 92, 49, 68, 72, 99, 54};
+            int[] sort_Marks = Marks;
+            int total = 0;
+            for(int i = 0; i < sort_Marks.Length - 1; i++)
+            {
+                for(int j = 0; j < sort_Marks.Length - i - 1; j++)
+                {
+                    if (sort_Marks[j] > sort_Marks[j + 1])
+                    {
+                        int temp = sort_Marks[j];
+                        sort_Marks[j] = sort_Marks[j + 1];
+                        sort_Marks[j + 1] = temp;
+                    }
+                }
+            }
+
+            for(int i =0; i < Marks.Length; i++)
+            {
+                total += Marks[i];
+            }
+            double avg = total/ Marks.Length;
+
+            for (int i = 0; i < Names.Length; i++)
+            {
+                for (int j = 0; j < sort_Marks.Length; j++)
+                {
+                    if(Marks.Length == sort_Marks.Length)
+                    {
+                        if (j > Marks.Length / 0.25)
+                        {
+
+                        }
+                    }
+                }
+
+                if (Marks[i] < (avg - 10))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Names[i] + " scored " + Marks[i].ToString());
+                }
+                else if (Marks[i] > (avg + 10))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(Names[i] + " scored " + Marks[i].ToString());
+                }
+                else
+                {
+                    Console.ForegroundColor= ConsoleColor.Yellow;
+                    Console.WriteLine(Names[i] + " scored " + Marks[i].ToString());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ W106 Lists/W109 Lists.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W106_Lists
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+
+        static void Print_list<T>(List<T> list)
+        {
+            Console.Write("[");
+            for (int i = 0; i < list.Count(); i++)
+            {
+                Console.Write((i == list.Count() - 1) ? list[i].ToString() : list[i].ToString() + ", ");
+            }
+            Console.Write("]");
+        }
+
+        static T[] listArray<T>(List<T> List)
+        {
+            T[] array = new T[List.Count];
+
+            for (int i = 0; i < List.Count(); i++)
+            {
+                array[i] = List[i];
+            }
+            return array;
+        }
+
+        static bool exist<T>(List<T> List, T obj)
+        {
+            bool exist = false;
+            for(int i = 0; i < List.Count(); i++)
+            {
+                if (obj.ToString() == List[i].ToString())
+                {
+                    exist = true;
+                }
+            }
+            return exist;
+        } 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what program to run?");
+            int prog_run;
+            do
+            {
+                prog_run = int.Parse(Console.ReadLine());
+                switch (prog_run)
+                {
+                    case 1:
+                        List<int> myList = new List<int>();
+
+                        for (int i = 0; i < 5; i++)
+                        {
+                            myList.Add(rnd.Next());
+                        }
+                        Print_list(myList);
+                        break;
+                    case 3:
+                        List<string> names = new List<string>();
+                        for (int i = 1; i < 6; i++)
+                        {
+                            Console.WriteLine("input name " + i + " :");
+                            string name = Console.ReadLine();
+                            if (!exist(names, name))
+                            {
+                                names.Add(name);
+                            }
+                            else
+                            {
+                                Console.WriteLine("they are already in the list");
+                            }
+                        }
+                        Print_list(names);
+                        break;
+
+                    case 4:
+                        Console.Write("write the words with a space inbetween : ");
+                        string ingredients = Console.ReadLine();
+
+                        break;
+                }
+            } while (prog_run != 1 && prog_run != 4 && prog_run != 3);
+            Console.ReadKey();
+        }
+    }
+}
