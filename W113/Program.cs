d0284ba90983b4464d4c27d290990588547b5b34
23JA1168 <23JA1168@L2768>
2023-11-24 15:59:42 +0000

    W113

---
+++ W113/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO
+
+namespace W113
+{
+    internal class Program
+    {
+        static void ConsoleLogs()
+        {
+            DateTime dt = new DateTime();
+            using (StreamWriter Sw = new StreamWriter("ConsoleLogs.txt", true))
+            {
+                Sw.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
