5b06e376d204bf83691ee9c8aa8da2e863a6644e
23JA1168 <23JA1168@L2768>
2023-12-14 10:16:00 +0000

    W116

---
+++ W116 Eceptions/W116 Exceptions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W116_Eceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static int[] getSides()
+        {
+            int[] sides = new int[2];
+            do
+            {
+                Console.WriteLine("the two side values?");
+                try
+                {
+                    sides[0] = parseSide(Console.ReadLine());
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine();
+                }
+            } while (sides.Length < 2);
+        }
+
+        static int parseSide(string side)
+        {
+            if (int.Parse(side) > sizeof(int))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            int output = 0;
+            output = int.Parse(side);
+            return output;
+        }
+    }
+}
