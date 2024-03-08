9ac1ad6ea275c15388f8a74488c976b34a1c7bc0
23JA1168 <23JA1168@L2757>
2024-03-08 15:48:04 +0000

    W131

---
+++ W130_P2/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W130_P2
+{
+    public class Employee
+    {
+        protected string forename, surname;
+        protected double salary;
+        protected Employee(string fullName, double startSalary)
+        {
+            string[] names = fullName.Split(' ');
+            forename = names[0];
+            surname = names[names.Length - 1];
+            salary = startSalary;
+        }
+        public void GiveRaise(double rate)
+        {
+            salary *= 1 + rate / 100;
+        }
+        public string GetFullName()
+        {
+            return forename + " " + surname;
+        }
+        public double GetMonthlyPay()
+        {
+            return Math.Round(salary / 12, 2);
+        }
+    }
+}
