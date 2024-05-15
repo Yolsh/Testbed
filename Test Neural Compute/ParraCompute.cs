9a00de57bcd37296888f0e687424faafe3b8d6fd
23JA1168 <23JA1168@L2377>
2024-05-15 14:05:35 +0100

    Attempt to repair nuget

---
+++ Test Neural Compute/ParraCompute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Neural_Compute
+{
+    public class ParraCompute
+    {
+        ParraCompute() 
+        {
+
+        }
+    }
+}
