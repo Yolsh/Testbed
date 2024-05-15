ec1648ca548287e20e0dfc72a41bfae5c07a9629
Yolsh <146455868+Yolsh@users.noreply.github.com>
2024-05-15 21:20:48 +0100

    testing GPU Compute

--- Test Neural Compute/ParraCompute.cs
+++ Test Neural Compute/ParraCompute.cs
@@ -3,14 +3,112 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharpDX;
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+using SharpDX.D3DCompiler;
 
 namespace Test_Neural_Compute
 {
     public class ParraCompute
     {
-        ParraCompute() 
-        {
+        private int groupSize;
+        private int totalSize;
+        private int elementByteSize;
 
+        private int[] parseData;
+        public int[] parsedData;
+
+        private Device device;
+        private ComputeShader cs;
+        private DeviceContext context;
+
+        private ShaderResourceView srvs;
+        private UnorderedAccessView uavs;
+        private SharpDX.Direct3D11.Buffer buffer;
+
+        public ParraCompute(int inGSize, int inTSize, int[] data) 
+        {
+            groupSize = inGSize;
+            totalSize = inTSize;
+            elementByteSize = 4;
+            parseData = data;
+            device = new Device(DriverType.Hardware, DeviceCreationFlags.SingleThreaded);
+        }
+
+        public void CompileFile(string FileName)
+        {
+            CompilationResult compiledHLSL = ShaderBytecode.CompileFromFile(FileName, "CSMain", "cs_5_0");
+            cs = new ComputeShader(device, compiledHLSL);
+        }
+
+        public void CompileString(string HLSL) 
+        {
+            CompilationResult compiledHLSL = ShaderBytecode.Compile(HLSL, "CSMain", "cs_5_0");
+            cs = new ComputeShader(device, compiledHLSL);
+        }
+
+        public void SetupBuffer()
+        {
+            BufferDescription bufferDescription = new BufferDescription()
+            {
+                SizeInBytes = elementByteSize * totalSize,
+                Usage = ResourceUsage.Default,
+                BindFlags = BindFlags.ShaderResource | BindFlags.UnorderedAccess,
+                OptionFlags = ResourceOptionFlags.BufferStructured,
+                StructureByteStride = elementByteSize,
+                CpuAccessFlags = CpuAccessFlags.Read,
+            };
+            buffer = SharpDX.Direct3D11.Buffer.Create(device, parseData, bufferDescription);
+
+            ShaderResourceViewDescription srvDesc = new ShaderResourceViewDescription()
+            {
+                Format = SharpDX.DXGI.Format.Unknown,
+                Dimension = ShaderResourceViewDimension.Buffer,
+                Buffer = new ShaderResourceViewDescription.BufferResource()
+                {
+                    ElementWidth = elementByteSize,
+                }
+            };
+            srvs = new ShaderResourceView(device, buffer, srvDesc);
+
+            UnorderedAccessViewDescription uavDesc = new UnorderedAccessViewDescription()
+            {
+                Format = SharpDX.DXGI.Format.Unknown,
+                Dimension = UnorderedAccessViewDimension.Buffer,
+                Buffer = new UnorderedAccessViewDescription.BufferResource()
+                {
+                    ElementCount = totalSize
+                }
+            };
+            uavs = new UnorderedAccessView(device, buffer, uavDesc);
+
+            context = device.ImmediateContext;
+            context.ComputeShader.Set(cs);
+
+            context.ComputeShader.SetConstantBuffer(0, buffer);
+            context.ComputeShader.SetShaderResource(0, srvs);
+            context.ComputeShader.SetUnorderedAccessView(0, uavs);
+        }
+
+        public int[] ReturnShaderResult()
+        {
+            int threadGroupCount = (totalSize + groupSize - 1) / groupSize;
+            context.Dispatch(threadGroupCount, 1, 1);
+            DataStream ds;
+            context.MapSubresource(buffer, MapMode.Read, MapFlags.None, out ds);
+            parsedData = ds.ReadRange<int>(parseData.Length);
+            return parsedData;
+        }
+
+        public void CloseCompute()
+        {
+            context.ClearState();
+            Utilities.Dispose(ref srvs);
+            Utilities.Dispose(ref uavs);
+            Utilities.Dispose(ref buffer);
+            Utilities.Dispose(ref cs);
+            Utilities.Dispose(ref device);
         }
     }
 }
--- Test Neural Compute/Program.cs
+++ Test Neural Compute/Program.cs
@@ -10,6 +10,26 @@ namespace Test_Neural_Compute
     {
         static void Main(string[] args)
         {
+            int[] vals = new int[100];
+            for (int i = 0; i < vals.Length; i++)
+            {
+                vals[i] = i;
+                Console.WriteLine(vals[i]);
+            }
+            Console.WriteLine();
+
+            ParraCompute compObj = new ParraCompute(2, 4, vals);
+
+            compObj.CompileFile("Shader.hlsl");
+            compObj.SetupBuffer();
+            vals = compObj.ReturnShaderResult();
+            compObj.CloseCompute();
+
+            for (int i = 0; i < vals.Length; i++)
+            {
+                Console.WriteLine(vals[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
