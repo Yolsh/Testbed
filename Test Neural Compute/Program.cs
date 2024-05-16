3340a4759493dd1e0f090eeeaf355e5184ef30c8
Yolsh <146455868+Yolsh@users.noreply.github.com>
2024-05-16 10:28:44 +0100

    change to string

--- Test Neural Compute/Program.cs
+++ Test Neural Compute/Program.cs
@@ -20,7 +20,15 @@ namespace Test_Neural_Compute
 
             ParraCompute compObj = new ParraCompute(2, vals);
 
-            compObj.CompileFile("Shader.hlsl");
+            compObj.CompileString(
+                @"RWStructuredBuffer<int> Result;
+
+[numthreads(2, 1, 1)]
+void CSMain(uint id : SV_DispatchThreadID)
+{
+    Result[id.x] = Result[id.x] * Result[id.x];
+}"
+);
             compObj.SetupBuffer();
             vals = compObj.ReturnShaderResult();
             compObj.CloseCompute();
