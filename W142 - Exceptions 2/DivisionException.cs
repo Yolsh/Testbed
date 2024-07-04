c0cfb302e3bd27ab97e87b42328c62a62e1865c4
23JA1168 <23JA1168@L2757>
2024-07-04 10:04:10 +0100

    W142

---
+++ W142 - Exceptions 2/DivisionException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W142___Exceptions_2
+{
+    public class DivisionException : DivideByZeroException
+    {
+        public DivisionException(int A) : base($"you attempted to divide {A} by 0") { }
+    }
+}
---
+++ W142 - Exceptions 2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W142___Exceptions_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new SaveFileException("Hello", "MyFile.txt");
+            }
+            catch (SaveFileException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+
+            bool cont = true;
+            do
+            {
+                Console.WriteLine("give 2 Numbers");
+                int A = int.Parse(Console.ReadLine());
+                int B = int.Parse(Console.ReadLine());
+                try
+                {
+                    Division(A, B);
+                }
+                catch (DivisionException ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("Press any key to end the Program");
+                    while (!Console.KeyAvailable)
+                    {
+                        Console.Write(".");
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                    cont = false;
+                }
+            } while (cont);
+        }
+
+        static double Division(int A, int B)
+        {
+            if (B == 0) throw new DivisionException(A);
+            return A / B;
+        }
+    }
+}
